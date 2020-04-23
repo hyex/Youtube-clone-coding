ba53ff34d9a4e75d3efec680ff7dd09418786d8d
hyex <hyexkim@gmail.com>
2020-04-24 03:16:44 +0900

    making videoDetailPage

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -51,7 +51,7 @@ function LandingPage() {
         var seconds = Math.floor((video.duration - minutes * 60));
 
         return <Col lg={6} md={8} xs={24} key={index}>
-            <a href={`/video/post/${video._id}`} >
+            <a href={`/video/${video._id}`} >
                 <div style={{ position: 'relative' }}>
                     <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
                     <div className=" duration">
